280ede36de0b634e5aadc27332a3cccba94ed9d0
Ben Lee <benjamintlee600@gmail.com>
2020-06-16 12:56:01 +0100

    added attributes to Student class.

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -1,4 +1,25 @@
 package studentdatabaseapp;
 
 public class Student {
+
+    private String firstName;
+    private String lastName;
+    private String year;
+    private int studentId;
+    private String courses;
+    private int tuitionBalance;
+    private int costOfCourse = 600;
+
+
+    // constructor: prompt user to enter students name and year
+
+    // generate an ID
+
+    // Enroll on courses
+
+    // view balance.
+
+    // pay tuition
+
+    // show status
 }
--- src/studentdatabaseapp/StudentDatabaseApp.java
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -2,6 +2,7 @@ package studentdatabaseapp;
 
 public class StudentDatabaseApp {
     public static void main(String[] args) {
-
+        // ask how many users we want to add.
+        // create n number of new students
     }
 }
