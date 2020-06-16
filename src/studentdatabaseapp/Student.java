56ec28f183dcc412bd804660467d2a9f8f5c12e5
Ben Lee <benjamintlee600@gmail.com>
2020-06-16 16:00:51 +0100

    added basic command line integration with student input.

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -1,14 +1,27 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class Student {
 
     private String firstName;
     private String lastName;
-    private String year;
+    private int year;
     private int studentId;
     private String courses;
     private int tuitionBalance;
     private int costOfCourse = 600;
+
+    public Student() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter student first name: ");
+        this.firstName = in.nextLine();
+        System.out.println("Enter student last name: ");
+        this.lastName = in.nextLine();
+        System.out.println("1 -  for 1st year\n2 - for 2nd year\n3 - for 3rd year\nEnter student year: ");
+        this.year = in.nextInt();
+        System.out.println(firstName + " " + lastName + " " + "year " + year);
+    }
 
 
     // constructor: prompt user to enter students name and year
--- src/studentdatabaseapp/StudentDatabaseApp.java
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -4,5 +4,6 @@ public class StudentDatabaseApp {
     public static void main(String[] args) {
         // ask how many users we want to add.
         // create n number of new students
+        Student stu1 = new Student();
     }
 }
