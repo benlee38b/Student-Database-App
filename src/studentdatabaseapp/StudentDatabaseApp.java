4cc767ae975fbfb9f476328d55fd6b1ce27c105d
Ben Lee <benjamintlee600@gmail.com>
2020-06-17 12:28:34 +0100

    added ability to select the number of students to enroll on the command line

--- src/studentdatabaseapp/StudentDatabaseApp.java
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -1,12 +1,24 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
     public static void main(String[] args) {
-        // ask how many users we want to add.
+        // ask how many students we want to add.
         // create n number of new students
-        Student stu1 = new Student();
-        stu1.enroll();
-        stu1.payTuition();
-        System.out.println(stu1.toString());
+
+        System.out.println("Enter number of new students to enroll: ");
+        Scanner in = new Scanner(System.in);
+        int numOfStudents = in.nextInt();
+        Student[] students = new Student[numOfStudents];
+
+        for(int n = 0; n < numOfStudents; n++) {
+            students[n] = new Student();
+            students[n].enroll();
+            students[n].payTuition();
+            System.out.println(students[n].toString());
+        }
+
+
     }
 }
